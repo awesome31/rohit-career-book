4b81418e4442fa56de46385a1a7e28ed35fd5431
Rohit Tyagi <rohit.tyagi@mosaicwellness.in>
2024-07-29 22:08:02 +0530

    Added chapters for algorithms

---
+++ algorithms/README.md
@@ -0,0 +1,13 @@
+# Algorithms
+
+## Time complexity
+
+1. Time complexity is the amount of time taken by an algorithm to run, as a function of the length of the input.
+2. It helps us make decisions about the efficiency of an algorithm.
+3. We always drop the constants and lower order terms.
+
+### Solving Problems
+
+1. [Linear Search](./kata-machine/src/day1/LinearSearchList.ts)
+2. [Binary Search](./kata-machine/src/day1/BinarySearchList.ts)
+3. [Two Crystal Balls](./kata-machine/src/day1/TwoCrystalBalls.ts)
--- algorithms/basics.ts
+++ algorithms/basics.ts
@@ -4,3 +4,17 @@
  * 2. It helps us make decisions about the efficiency of an algorithm.
  * 3. We always drop the constants and lower order terms.
  */
+
+/**
+ * Arrays:
+ * 1. Arrays are contiguous blocks of memory of a fixed size.
+ * 2. In traditional arrays, the size is fixed. int32[3] => This means that the array can hold 3 integers.
+ * 3. Once the array size is fixed, it cannot be changed.
+ */
+
+/**
+ * Linear Search:
+ * 1. Linear search is a simple search algorithm that searches for a target value within an array.
+ * 2. It compares each element of the array to the target value until a match is found.
+ * 3. The time complexity of linear search is O(n).
+ */
