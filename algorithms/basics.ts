9d79d7e5353396972abd06e8af035efadbad133d
Rohit Tyagi <rohit.tyagi@mosaicwellness.in>
2024-07-29 10:10:07 +0530

    Add chapters for rust, advent-of-code and algorithms

---
+++ algorithms/basics.ts
@@ -0,0 +1,6 @@
+/**
+ * Time complexity.
+ * 1. Time complexity is the amount of time taken by an algorithm to run, as a function of the length of the input.
+ * 2. It helps us make decisions about the efficiency of an algorithm.
+ * 3. We always drop the constants and lower order terms.
+ */
