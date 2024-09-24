d8ef06f78cda3f3fb8b84b7e8b53ef0ed15033c5
Rohit Tyagi <rohit.tyagi@mosaicwellness.in>
2024-09-24 17:31:07 +0530

    Add twoth project and add chapter for ebs

---
+++ twoth/twoth-dentist/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/tyagiro31/Documents/learn/career-book/twoth/twoth-dentist/src/app/layout\":[\"static/media/463dafcda517f24f-s.p.woff\",\"static/media/4473ecc91f70f139-s.p.woff\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
