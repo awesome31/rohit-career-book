9ab1c5f8bbbb67f9e082e3ffdfd35a061b799e79
Rohit Tyagi <rohit.tyagi@mosaicwellness.in>
2024-10-03 12:17:32 +0530

    Solve daily coding problem 9

--- daily-coding-problems/day9.js
+++ daily-coding-problems/day9.js
@@ -3,3 +3,24 @@
 
 For example, [2, 4, 6, 2, 5] should return 13, since we pick 2, 6, and 5. [5, 1, 1, 5] should return 10, since we pick 5 and 5.
  */
+
+/**
+ *
+ * @param {number[]} arr
+ */
+function findLargestSumNonAdjacent(arr) {
+  if (arr.length === 1) {
+    return Math.max(0, arr[0]);
+  } else if (arr.length === 2) {
+    return Math.max(0, arr[0], arr[1]);
+  }
+
+  return Math.max(
+    arr[0] + findLargestSumNonAdjacent(arr.slice(2)),
+    findLargestSumNonAdjacent(arr.slice(1)),
+    0
+  );
+}
+
+console.log(findLargestSumNonAdjacent([2, 4, 6, 2, 5]));
+console.log(findLargestSumNonAdjacent([-22, -2, -2, 2, -1, 5]));
